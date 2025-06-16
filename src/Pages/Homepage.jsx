9cb7293dc8f4a460dec062a59bcf0bc45fb7095d
Anovardhan <anovardhan18@gmail.com>
2025-06-17 00:06:46 +0530

    f

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -178,7 +178,7 @@ const filteredProducts = products.filter(product => {
   </div>
 </div>
 
-<div className='row'>
+<div className='row ' style = {{marginLeft:"100px"}}>
     {
         filteredProducts.map((product)=>(
             <div className="col-md-4 p-4" key={product.id}  style={{cursor:"pointer"}}>
